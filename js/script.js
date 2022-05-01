a5d411565b59a2309e4e5e3f1abd2dc9899ecc08
Akorede <akoredeyk@gmail.com>
2022-05-01 17:39:38 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-function light(show) {
-    //body....
-    var pic;
-    if (show == 0) {
-        pic = "pic_bulboff.gif";
-    }
-    else{
-        pic = "pic_bulbon.gif";
-    }
-    document.getElementById('bulb').src = pic;
-}
+
